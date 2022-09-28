47ddece942ef665a8fd47cf446979c19ff21b720
gmoss406 <85966365+gmoss406@users.noreply.github.com>
2022-09-28 11:39:59 -0600

    Delete littleR2 directory

--- littleR2/radiosonde2littler.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-for FILE in villarrica/*
-do
-	echo $FILE
-	date=$(echo $FILE | awk -F '[_.]' '{print $3 20 $2 0 0}')
-	#echo $date
-	littler -n $FILE -i '' -d $date -s balloon -o $FILE'.littler' -t graw $FILE
-done
